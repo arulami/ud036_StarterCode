3bf4b2838546a22ee5191583d2dacf67d94f3976
arulami <arulami@gmail.com>
2017-08-25 14:25:08 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Movie trailer website 
-Source code for a Movie Trailer website using Python
+  Movie trailer website is a simple website to show a bunch of movies with titles, name and with trailer. 
 
+## Installation
+    
+   - Install [Python](https://www.python.org)
+   - Clone the GitHub repository 
+   
+   $ git clone https://github.com/arulami/ud036_StarterCode.git
+   
+   $ cd ud036_StarterCode
+   
+   $ python entertainment_center.py
--- media.py
+++ media.py
@@ -1,14 +1,19 @@
 import webbrowser
 
+
 class Movie():
+    """A Movie class with movie details"""
+
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """Initialize the variables"""
+
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-
     def show_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
+        """Open youtube trailer url and shows the trailer"""
 
+        webbrowser.open(self.trailer_youtube_url)
